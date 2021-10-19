532f2c39321074972b84676ff128440761bacbf3
LauchaQ2 <89219479+LauchaQ2@users.noreply.github.com>
2021-10-19 14:29:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+ScrollReveal().reveal('.title', { 
+    distance: '50px',
+    delay: 1000});
+
+
+function nota1(){
+    ScrollReveal().reveal('#lista1', { 
+    distance: '0px',
+    delay: 0});
+    ScrollReveal().reveal('.imagen1', { 
+    distance: '50px',
+    delay: 500});
+    ScrollReveal().reveal('.subtitle1', { 
+    distance: '50px',
+    delay: 1000});
+    ScrollReveal().reveal('#title1', { 
+    distance: '50px',
+    delay: 1500});
+    ScrollReveal().reveal('#by1', { 
+    distance: '20px',
+    delay: 2000});
+}
+function nota2(){
+    ScrollReveal().reveal('.imagen2', { 
+    distance: '50px',
+    delay: 500});
+    ScrollReveal().reveal('.subtitle2', { 
+    distance: '50px',
+    delay: 1000});
+    ScrollReveal().reveal('#title2', { 
+    distance: '50px',
+    delay: 1500});
+    ScrollReveal().reveal('#by2', { 
+    distance: '20px',
+    delay: 2000});
+}
+function nota3(){
+    ScrollReveal().reveal('.imagen3', { 
+    distance: '50px',
+    delay: 500});
+    ScrollReveal().reveal('.subtitle3', { 
+    distance: '50px',
+    delay: 1000});
+    ScrollReveal().reveal('#title3', { 
+    distance: '50px',
+    delay: 1500});
+    ScrollReveal().reveal('#by3', { 
+    distance: '20px',
+    delay: 2000});
+}
+
